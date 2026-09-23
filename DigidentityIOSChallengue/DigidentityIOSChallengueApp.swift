06af3fe8a3ca106fd98c856cf02ae3f484f7467d
elblonder2013 <elblonder2013@gmail.com>
2026-09-23 11:47:10 +0200

    Setup blank project

---
+++ DigidentityIOSChallengue/DigidentityIOSChallengueApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigidentityIOSChallengueApp.swift
+//  DigidentityIOSChallengue
+//
+//  Created by Alexei on 23/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct DigidentityIOSChallengueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
